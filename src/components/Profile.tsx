6334dbac543c7de83bd85b1092e846546920d5b6
Enmanuel <end199615@gmail.com>
2025-06-10 21:29:46 -0500

    Ruta relativa para imagen de perfil

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,33 +1,34 @@
 import { Typewriter } from "react-simple-typewriter";
+import profile from "../assets/images/profile.png";
 
 type ProfileProps = {
   theme: string;
 };
 
 function Profile({ theme }: ProfileProps) {
-    return (
-        <>
-         <div className="row align-items-center justify-content-center w-100 h-100">
-          <div className="col-lg-8 text-center">
-            <h1 className="display-4 mb-3">Hola, soy Enmanuel</h1>
-            <h3 className="text-primary">
-              <Typewriter
-                words={['Desarrollador Fullstack', 'Estudiante de ingeniería de software con IA', 'Apasionado por el código.']}
-                loop={0} // 0 = infinito
-                cursor
-                cursorStyle="|"
-                typeSpeed={70}
-                deleteSpeed={50}
-                delaySpeed={2000}
-              />
-            </h3>
-          </div>
-          <div className="perfil col-lg-4 justify-content-center">
-            <img src="src/assets/images/profile.png"
-              style={{ border: `2px solid ${theme === 'dark' ? '#fff' : '#000'}` }} />
-          </div>
+  return (
+    <>
+      <div className="row align-items-center justify-content-center w-100 h-100">
+        <div className="col-lg-8 text-center">
+          <h1 className="display-4 mb-3">Hola, soy Enmanuel</h1>
+          <h3 className="text-primary">
+            <Typewriter
+              words={['Desarrollador Fullstack', 'Estudiante de ingeniería de software con IA', 'Apasionado por el código.']}
+              loop={0} // 0 = infinito
+              cursor
+              cursorStyle="|"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={2000}
+            />
+          </h3>
         </div>
-        </>
-    )
+        <div className="perfil col-lg-4 justify-content-center">
+          <img src={profile} alt="Perfil"
+            style={{ border: `2px solid ${theme === 'dark' ? '#fff' : '#000'}` }} />
+        </div>
+      </div>
+    </>
+  )
 }
 export default Profile;
